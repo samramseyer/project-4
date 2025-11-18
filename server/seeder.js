e914ef1d89f51dbfcb9a3db408ee2247815336fb
samramseyer <sam.ramseyer.88@gmail.com>
2025-11-17 18:02:00 -0600

    Fix deprecated MongoDB connection options
    - Remove useNewUrlParser option (deprecated in Mongoose 6+)
    - Remove useUnifiedTopology option (deprecated in Mongoose 6+)
    - Clean up connection warnings

--- server/seeder.js
+++ server/seeder.js
@@ -12,10 +12,7 @@ const Question = require('./models/Question');
 const Answer = require('./models/Answer');
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Sample data
 const categories = [
