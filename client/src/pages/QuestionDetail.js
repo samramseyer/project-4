28bd049e027ec852fa2af2c1b4d57bab23b540b1
samramseyer <sam.ramseyer.88@gmail.com>
2025-11-17 18:02:28 -0600

    Fix React Hook dependency warnings
    - Add eslint-disable comments for useEffect hooks
    - Prevent infinite loops from function dependencies
    - Maintain proper component lifecycle behavior

--- client/src/pages/QuestionDetail.js
+++ client/src/pages/QuestionDetail.js
@@ -17,6 +17,7 @@ const QuestionDetail = () => {
   useEffect(() => {
     fetchQuestion();
     fetchAnswers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const fetchQuestion = async () => {
